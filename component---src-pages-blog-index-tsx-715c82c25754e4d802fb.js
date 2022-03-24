48cd8d2e8e39ae967e68f6bb211b2f907641f6f4
jadry92 <jhoanseb1992@gmail.com>
2022-03-24 04:19:06 +0000

    Deploying to gh-pages from @ jadry92/BlogNotes@57e2fca6d691e0b442dee9114dd394d06a457ef9 🚀

---
+++ component---src-pages-blog-index-tsx-715c82c25754e4d802fb.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[643],{8628:function(e,t,l){l.r(t);var n=l(7294),r=l(1597);t.default=function(e){var t=e.data;return n.createElement("div",null,t.allMdx.nodes.map((function(e){return n.createElement("article",{key:e.id},n.createElement("h2",null,e.frontmatter.title),n.createElement("p",null,"Posted: ",e.frontmatter.date),n.createElement(r.rU,{to:"/blog/"+e.slug},e.frontmatter.title))})))}}}]);
+//# sourceMappingURL=component---src-pages-blog-index-tsx-715c82c25754e4d802fb.js.map
