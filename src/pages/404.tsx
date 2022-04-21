ef44dc2c8628bb694b619f8ec75a335a4e7dc35f
johan suarez <johanseb1992@gmail.com>
2022-04-22 00:05:28 +1000

    impruving 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,52 +1,21 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
 
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
+    <main className="fluid-container bg-color-3 text-white min-vh-100 flex-column d-flex justify-content-center align-items-center">
       <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
+      <h1 className="mb-4">Page not found 404</h1>
+      <p>
         Sorry{" "}
         <span role="img" aria-label="Pensive emoji">
           😔
         </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
+        we couldn't find what you were looking for.
       </p>
+      <Link to="/">Go home</Link>
     </main>
   )
 }
